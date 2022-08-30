4a161b554d602c76e77f1f1ec3416f0b510a8b3b
Gian Carlo De Castro <Giancld92@gmail.com>
2022-08-30 08:42:00 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (blocks) {
+    if (blocks > 42)
+        return blocks - 42
+    else (blocks < 42)
+        return 42 - blocks
+}
+
+function distanceFromHqInFeet (feet) {
+    if (feet > 42)
+        return (feet - 42) * 264
+    else (feet < 42)
+        return (42 - feet) * 264
+}
+
+function distanceTravelledInFeet (start0, destination0) {
+    if (start0 > destination0)
+        return (start0 - destination0) * 264
+    else (start0 < destination0)
+        return (destination0 - start0) * 264
+}
+
+function calculatesFarePrice (start, destination) {
+    let block = start - destination
+    let travel = Math.abs(block) * 264
+    if (travel <= 400) {
+        return 0
+    }
+    else if (travel > 400 && travel <= 2000) {
+        return (travel - 400) * 0.02
+    }
+    else if (travel > 2000 && travel <= 2500) {
+        return 25
+    }
+    else if (travel > 2500) {
+        return "cannot travel that far"
+    }
+}
